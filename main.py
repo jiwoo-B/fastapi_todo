0426e75ff717b9a20480604e728f622f1d725d15
jiwoo <jiwoo7190@naver.com>
2024-06-03 18:03:54 +0900

    retry

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import uvicorn
 
 app = FastAPI()
 
-origins = ["http://127.0.0.1:5500", "http://52.71.209.15"]
+origins = ["http://127.0.0.1:5500", "http://52.71.209.15:8081"]
 
 app.add_middleware(
     CORSMiddleware,
